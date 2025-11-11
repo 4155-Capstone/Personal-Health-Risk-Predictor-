8d3c587658586b7538539abe0fa245be5fa7a97b
Karunya01 <karthikeyankarunya@gmail.com>
2025-11-11 18:52:42 -0500

    Adding UI file for Multi-disease prediction

---
+++ app_ui.py
@@ -0,0 +1,21 @@
+import streamlit as st
+from heart_disease_app import heart_ui
+from diabetes_app import diabetes_ui
+
+st.set_page_config(page_title="Multi-disease Risk Checker", layout="centered")
+st.title("🩺 Multi-disease Risk Checker")
+st.markdown("**Educational tool only — not a medical diagnosis.**")
+
+# Select model
+model_choice = st.radio(
+    "Select which disease risk to check:",
+    ("Heart Disease", "Diabetes"),
+    horizontal=True  # This makes the radio buttons appear side by side
+)
+
+
+# Call the corresponding UI function
+if model_choice == "Heart Disease":
+    heart_ui()
+else:
+    diabetes_ui()
